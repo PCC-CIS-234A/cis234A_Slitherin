51ea080545123430231c608db7bfb9d203f0593f
Mike <triggermikedj@gmai.com>
2023-04-19 16:34:00 -0700

    Initial Commit

---
+++ Database.py
@@ -0,0 +1,22 @@
+import pyodbc
+
+
+class Database:
+    __connection = None
+
+    # Establish connection to database
+    @classmethod
+    def connect(cls):
+        """This method connects to the database"""
+
+        if cls.__connection is None:
+            server = 'tcp:cisdbss.pcc.edu'
+            database = 'cis234A_Slitherin'
+            username = 'cis234A_Slitherin'
+            password = 'CIS234ATeamTry2!'
+
+            cls.__connection = pyodbc.connect(
+                'DRIVER={SQL Server};SERVER=' + server
+                + ';DATABASE=' + database
+                + ';UID=' + username + ';PWD=' + password
+            )
---
+++ WebUI.py
@@ -0,0 +1,131 @@
+from flask import Flask, render_template, redirect, url_for, request
+from Template import Template
+
+
+class WebUI:
+    """Class definition for web UI -
+    View the output on localhost:5000/"""
+
+    __app = Flask(__name__)
+    __template_list = Template.build_list()
+
+    def __init__(self):
+        self.__app.secret_key = self.generate_key()
+
+    @staticmethod
+    def generate_key():
+        """This method generates a cryptographically random session key"""
+
+        import os
+
+        return os.urandom(6)
+
+    @staticmethod
+    @__app.route("/menu")
+    @__app.route("/home")
+    @__app.route("/default")
+    @__app.route("/default.html")
+    def redirect_to_menu():
+        """This method redirects common homepage URLs to homepage"""
+
+        return redirect(url_for("homepage"))
+
+    @staticmethod
+    @__app.route("/")
+    def homepage():
+        """This method displays the homepage"""
+
+        return render_template("login.html")
+
+    @staticmethod
+    @__app.route("/landing_page")
+    def index():
+        """This method displays the landing page after login"""
+
+        return render_template("landing_page.html")
+
+    @staticmethod
+    @__app.route("/under_construction.html")
+    @__app.route("/under_construction")
+    def under_con():
+        return render_template("under_construction.html")
+
+    @staticmethod
+    @__app.route("/create_notification")
+    def send_notification():
+        """This method allows the user to send a food availability
+        notification to all subscribers"""
+
+        # TODO: REMOVE TEST CODE
+
+        return render_template("create_notification.html", template_list=WebUI.__template_list)
+
+    @staticmethod
+    @__app.route("/create_template")
+    def create_template():
+        """This method allows a user to create a template"""
+
+        return render_template("under_construction.html")
+
+    @staticmethod
+    def find_template(template_name):
+        """This method takes a list name and returns a list object"""
+
+        for template in WebUI.__template_list:
+            if template.get_key() == template_name.lower():
+                return template
+
+        return None
+
+    @staticmethod
+    @__app.route("/use_template")
+    def use_template():
+        """This method allows the use of a Template to send a notification"""
+
+        template_name = request.args['template']
+        template = WebUI.find_template(template_name)
+
+        if template is None:
+            return render_template(
+                "error.html",
+                error_message=f"There is no template named '{template_name}'"
+            )
+
+        return render_template("use_template.html", template=template)
+
+    @staticmethod
+    @__app.route("/send_notification")
+    def send_not():
+        subject = request.args['subject']
+        msg = request.args['message']
+
+        # TODO: ADD SEND FUNCTION HERE AND ASK HOW TO GET MESSAGE (cont)
+        # TODO: WITHOUT THE ENTIRE MESSAGE TEXT BEING IN THE URL - POST METHOD?
+
+        return render_template("send_success.html", subject=subject)
+
+    @staticmethod
+    @__app.route("/send_success")
+    def send_success():
+        # TODO: ADD TO LOG HERE
+        template = request.args["subject"]
+
+        return render_template("send_success.html")
+
+    @staticmethod
+    @__app.route("/review_log")
+    def view_log():
+        """This method allows a user to view the notification log"""
+
+        return render_template("under_construction.html")
+
+    @staticmethod
+    def run():
+        """This method runs the UI"""
+
+        WebUI.__app.run(port=5000)
+
+
+if __name__ == "__main__":
+    app = WebUI()
+    app.run()
