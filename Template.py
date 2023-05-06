60b1f5d1ced07c87517021a0bd69aac14e94b13b
Tarin Aguirre <tarin.aguirre@pcc.edu>
2023-05-05 18:58:37 -0700

    updated template py

--- Template.py
+++ Template.py
@@ -1,3 +1,6 @@
+from Database import Database
+
+
 class Template:
     def __init__(self, name, subject, text):
         self.__name = name
@@ -7,34 +10,20 @@ class Template:
     def __str__(self):
         return self.__name
 
+    def get_name(self):
+        return self.__name
+
+    def get_subject(self):
+        return self.__subject
+
+    def get_text(self):
+        return self.__text
+
     def get_key(self):
         return self.__name.lower()
 
     @staticmethod
     def build_list():
-        temp1 = Template(
-            'Default',
-            'Food Available!',
-            'Food is now available at Panther Pantry!\n'
-        )
+        """This method builds a list of templates"""
 
-        temp2 = Template(
-            'Perishable',
-            'Perishable Food Available!',
-            'Perishable food is now available at Panther Pantry!\n'
-            '\nGet it while it\'s good!'
-        )
-
-        temp3 = Template(
-            name='New Item',
-            subject='New Item Available!',
-            text='There is a new item available at Panther Pantry!\n'
-            '\nWe now have <BLANK>. Come and get your <BLANK>!'
-        )
-
-        template_list = [temp1, temp2, temp3]
-
-        return template_list
-
-    def get_name(self):
-        return self.__name
+        return Database.build_template_list()
