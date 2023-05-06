652a72b28c7214dd70e97731e1dd7ebe386e4f4c
Tarin Aguirre <tarin.aguirre@pcc.edu>
2023-05-05 19:03:51 -0700

    updated webui

--- WebUI.py
+++ WebUI.py
@@ -33,7 +33,7 @@ class WebUI:
     @staticmethod
     @__app.route("/")
     def homepage():
-        """This method displays the homepage"""
+        """This method displays the login page"""
 
         return render_template("login.html")
 
@@ -45,32 +45,35 @@ class WebUI:
         return render_template("landing_page.html")
 
     @staticmethod
-    @__app.route("/under_construction.html")
     @__app.route("/under_construction")
-    def under_con():
+    def under_construction():
+        """This method displays the construction page"""
+
         return render_template("under_construction.html")
 
     @staticmethod
     @__app.route("/create_notification")
-    def send_notification():
-        """This method allows the user to send a food availability
-        notification to all subscribers"""
+    def create_notification():
+        """This method allows the user to send a notification to
+        all subscribers"""
 
-        # TODO: REMOVE TEST CODE
-
-        return render_template("create_notification.html", template_list=WebUI.__template_list)
+        return render_template(
+            "create_notification.html",
+            template_list=WebUI.__template_list
+        )
 
     @staticmethod
     @__app.route("/create_template")
     def create_template():
         """This method allows a user to create a template"""
 
-        return render_template("create_template.html")
+        return render_template("under_construction.html")
 
     @staticmethod
     def find_template(template_name):
-        """This method takes a list name and returns a list object"""
+        """This method takes a template name and returns a template object"""
 
+        # Search template list for template
         for template in WebUI.__template_list:
             if template.get_key() == template_name.lower():
                 return template
@@ -80,37 +83,61 @@ class WebUI:
     @staticmethod
     @__app.route("/use_template")
     def use_template():
-        """This method allows the use of a Template to send a notification"""
+        """This method allows the use of a template to send a notification"""
 
+        # Find template object using name from input
         template_name = request.args['template']
         template = WebUI.find_template(template_name)
 
+        # If no template found, raise error
         if template is None:
             return render_template(
                 "error.html",
                 error_message=f"There is no template named '{template_name}'"
             )
 
-        return render_template("use_template.html", template=template)
+        return render_template(
+            "use_template.html",
+            template_name=template.get_name(),
+            template_subject=template.get_subject(),
+            template_text=template.get_text()
+        )
 
     @staticmethod
     @__app.route("/send_notification")
-    def send_not():
+    def send_notification():
+        """This method gathers information from the UI for sending a
+        notification"""
+
+        from Email import Email
+        from Log import Log
+        from datetime import datetime
+
+        # Collect required information
+        to_addresses = Email.get_email_list()
+        from_address = "PantherPantry.PCC.01@gmail.com"
         subject = request.args['subject']
-        msg = request.args['message']
+        body = request.args['message']
+        count = len(to_addresses)
+        time_sent = datetime.now()
 
-        # TODO: ADD SEND FUNCTION HERE AND ASK HOW TO GET MESSAGE (cont)
-        # TODO: WITHOUT THE ENTIRE MESSAGE TEXT BEING IN THE URL - POST METHOD?
+        # Send emails
+        Email.send_email(
+            from_address,
+            to_addresses,
+            subject,
+            body
+        )
 
-        return render_template("send_success.html", subject=subject)
+        # Send notification to log
+        Log.send_to_db(subject, body, 'change me', time_sent, count)
 
-    @staticmethod
-    @__app.route("/send_success")
-    def send_success():
-        # TODO: ADD TO LOG HERE
-        template = request.args["subject"]
-
-        return render_template("send_success.html")
+        return render_template(
+            "send_success.html",
+            subject=subject,
+            time=time_sent,
+            send_count=count
+        )
 
     @staticmethod
     @__app.route("/review_log")
