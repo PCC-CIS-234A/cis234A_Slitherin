5669417488f0c7cdf74858418acf9e6529ab1995
Tarin Aguirre <tarin.aguirre@pcc.edu>
2023-05-06 13:37:00 -0700

    reformatted adddb

--- AddDB.py
+++ AddDB.py
@@ -1,14 +1,12 @@
 from Database import Database
 
+
 class AddDB:
-    def __init__(self, subject, message, sender_id, time_sent, count):
+    def __init__(self, name, subject, message):
+        self.name = name
         self.subject = subject
         self.message = message
-        self.sender_id = sender_id
-        self.time_sent = time_sent
-        self.send_count = count
 
     @staticmethod
     def add_to_db(name, subject, message):
-
         Database.create_template(name, subject, message)
