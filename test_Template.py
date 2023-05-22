da2eb21258a1d7af0d4af407b3896628388a9070
Tarin Aguirre <tarin.aguirre@pcc.edu>
2023-05-22 16:00:00 -0700

    created test class

---
+++ test_Template.py
@@ -0,0 +1,19 @@
+import unittest
+from Template import Template
+
+
+class TestTemplate(unittest.TestCase):
+    def test_get_name(self):
+        self.fail()
+
+    def test_get_subject(self):
+        self.fail()
+
+    def test_get_text(self):
+        self.fail()
+
+    def test_get_key(self):
+        self.fail()
+
+    def test_build_list(self):
+        self.fail()
