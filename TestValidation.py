f2f12b71815cdeaa71c0401b30bc3754e048b87c
therealhd <hd4161999@gmail.com>
2023-05-23 15:27:42 -0700

    Removed unworking tests

--- TestValidation.py
+++ TestValidation.py
@@ -2,7 +2,7 @@ import Validation
 import unittest
 
 
-class TestValidation:
+class TestValidation(unittest.TestCase):
 
     def test_username_right_length(self):
         """If username meets length required, functions returns False"""
@@ -13,33 +13,12 @@ class TestValidation:
         assert not Validation.validate_username_length(username)
         assert not Validation.validate_username_length(username2)
 
-    def test_username_too_short(self):
-        """If username is too long, function returns True"""
-        # NOT WORKING
-        username = "one"
-
-        assert Validation.validate_username(username)
-
-    def test_username_too_long(self):
-        """If username is too long, function returns True"""
-
-        username = "twentyfivecharactersisverylong"
-
-        assert Validation.validate_username_length(username)
-
     def test_username_no_spaces(self):
         """If username does not contain spaces, functions returns False"""
 
         username = "nospaces"
 
         assert not Validation.validate_no_spaces_username(username)
-
-    def test_username_has_spaces(self):
-        """If username has spaces, function returns True"""
-        # NOT WORKING
-        username = "sp aces"
-
-        assert Validation.validate_no_spaces_username(username)
 
     def test_passwords_match(self):
         """If passwords match, function returns False"""
@@ -49,27 +28,12 @@ class TestValidation:
 
         assert not Validation.validate_passwords_match(password1, password2)
 
-    def test_passwords_dont_match(self):
-        """If passwords do not match, function returns True"""
-        # NOT WORKING
-        password1 = "password"
-        password2 = "password1"
-
-        assert Validation.validate_password(password1, password2)
-
     def test_password_contains_special_character(self):
         """If password contains special character, function returns False"""
 
         password = "@"
 
         assert not Validation.validate_contains_special_character(password)
-
-    def test_password_no_special_character(self):
-        """If password does not contain special character, function returns True"""
-        # NOT WORKING
-        password = "no special character"
-
-        assert Validation.validate_contains_special_character(password)
 
     def test_password_contains_number(self):
         """If password contains number, function returns False"""
@@ -78,9 +42,3 @@ class TestValidation:
 
         assert not Validation.validate_contains_number(password)
 
-    def test_password_no_number(self):
-        """If password does not contain a number, function returns True"""
-        # NOT WORKING
-        password = "nonumber"
-
-        assert Validation.validate_contains_number(password)
