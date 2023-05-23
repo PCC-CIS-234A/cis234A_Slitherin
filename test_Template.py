88ddbb601e39ab8b766d91bebaf1f72b62bfd187
Tarin Aguirre <tarin.aguirre@pcc.edu>
2023-05-22 18:34:22 -0700

    no red squiggles

--- test_Template.py
+++ test_Template.py
@@ -9,12 +9,12 @@ class TestTemplate(unittest.TestCase):
     def test_get_name(self):
         name = 1
         subject = 2
-        test = 3
+        text = 3
         key = 4
 
-    template = Template(name, subject, text, key)
+        template = Template(name, subject, text, key)
 
-    self.assertEqual(name, template.get_name())
-    self.assertEqual(subject, template.get_subject())
-    self.assertEqual(text, template.get_text())
-    self.assertEqual(key, template.get_key())
+        self.assertEqual(name, template.get_name())
+        self.assertEqual(subject, template.get_subject())
+        self.assertEqual(text, template.get_text())
+        self.assertEqual(key, template.get_key())
